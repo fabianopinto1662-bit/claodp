d718d53f3a941d76babbc3f63b10db60459b4ed7
fabianopinto1662-bit <fabianopinto1662-bit@gmail.com>
2026-09-11 12:10:19 -0500

    apk 7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.actualiza-ahora-movil.store/";
+const SITE_URL = "https://www.actualizacion-web.help/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
