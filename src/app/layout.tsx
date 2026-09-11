71dddb12b5066c87786e71e2fa3a0ba1d5e74d7a
fabianopinto1662-bit <fabianopinto1662-bit@gmail.com>
2026-09-11 11:13:54 -0500

    apk 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.descarga-tu-app.online/";
+const SITE_URL = "https://www.actualiza-ahora-movil.store/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
