5df431190f74ac9af965b4e08ef6f58bf7187b81
fabianopinto1662-bit <fabianopinto1662-bit@gmail.com>
2026-09-23 16:00:06 -0500

    apk 22

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.actualiza-tumovil.online/";
+const SITE_URL = "https://www.actualizate-movil5g.lat/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
